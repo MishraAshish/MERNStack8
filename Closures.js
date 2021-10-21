db1fc8401c6f648098ff3e58ee8cf23aa3f3950e
Ashish <ash.ishu025g@gmail.com>
2021-10-21 21:13:41 +0530

    closure

---
+++ Closures.js
@@ -0,0 +1,34 @@
+//Closures : 
+//encapsulation in JS : we decide what props and methods are accessible to the outer world
+//closure : we have parent child relation where parent function returns a child and the space between them
+// can be used as public or private, thus givin us an encapsulation like behaviour
+
+
+function Parent(name, age) {
+    var name = name; //public
+    var age = age; //public
+    var accountBalance = "$1000"; //private
+    var accountPin = "23xyzn9"; //private
+    var accountType = "saving"; //public
+
+    var Child = function (address, isAdmin) {
+
+        return {
+                    //name : name // shorthand while using es6 we can use only one of it, if the key and the variable name both are same
+                    name,
+                    age,
+                    accountType,
+                    address
+            }
+    }
+
+    return Child;
+}
+
+var parentObj = Parent("Ciara",21);
+
+//console.log(parentObj)
+console.log(parentObj("Somewhere on earth", false))
+
+// pass the isAdmin and depending upon true or false : if true then return pin and amount as well
+// create a closure with an example of person being parent and student being child
