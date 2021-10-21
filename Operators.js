0e6e481c9aa9df4415d1033092bcbf7a6132c31e
Ashish <ash.ishu025g@gmail.com>
2021-10-21 22:00:34 +0530

    operators

---
+++ Operators.js
@@ -0,0 +1,16 @@
+//operator : and, or, not
+
+// &&, ||, !, ==, ===, !==, <, >, ? (ternary)
+
+var age = 18;
+var validCitizen = true;
+
+//if (validCitizen || age == 18) {
+if (validCitizen && age === "18") { //== compares values only but === compares type as well
+    console.log("Allowed to vote")
+}else{
+    console.log("Not Allowed to vote")
+}
+
+//ternary operators
+validCitizen && age === "18" ?console.log("Allowed to vote ternary") : console.log("Not Allowed to vote ternary");
