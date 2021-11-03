55d081542c1958ba91e22c73040df50e44366294
Ashish <ash.ishu025g@gmail.com>
2021-11-03 22:54:22 +0530

    object destructuring

--- es6/destructuring.js
+++ es6/destructuring.js
@@ -19,18 +19,59 @@
 
 //c. rest the variables
 
- let [one, two, three, ...restAll] = [1,2,3,4,5,6,7]
-
-console.log(one)
-console.log(two)
-console.log(three)
-console.log(restAll)
+//let [one, two, three, ...restAll] = [1,2,3,4,5,6,7]
+// console.log(one)
+// console.log(two)
+// console.log(three)
+// console.log(restAll)
 
 
 //d. swapping of variables
 
-let morning = "sun", evening =  "moon";
+//let morning = "sun", evening =  "moon";
+// [morning, evening] = [evening, morning] //swapping of variables
+// console.log(morning)
+// console.log(evening)
 
-[morning, evening] = [evening, morning] //swapping of variables
-console.log(morning)
-console.log(evening)
+
+//2. Object Destructuring
+
+// assume below object you are going to get from an api (getstudent)
+let Student = {
+    name : "Yourui",
+    id : 21,
+    subjects : { 
+        maths : 25,
+        chemistry : 26,
+        biology : 27
+    },
+    hostel : "some hostel",
+    room : 209
+}
+
+// let name = Student.name;
+// let maths = Student.subjects.name;
+// let chemistry = Student.subjects.name;
+// let physics = 26;
+
+let {name, subjects:{maths, chemistry, physics = 29}} = Student;
+
+console.log(name)
+console.log(maths)
+console.log(chemistry)
+console.log(physics)
+
+
+//question :
+
+let Student = {
+    firstname : "Put your name",
+    address : " ??",
+    sessionTopics : {
+        covered1 : "CoreJS",
+        covered2 : "OOJS"
+    }
+}
+
+//print firstname and sessionTopics, 
+//along with that also create a lastname and covered3 as "ES6", without making any change in student
