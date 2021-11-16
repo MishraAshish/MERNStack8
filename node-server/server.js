040dccfd17b645509a31c5cf779171c838c49f84
Ashish <ash.ishu025g@gmail.com>
2021-11-16 23:35:17 +0530

    nodemon query-string route-params

--- node-server/server.js
+++ node-server/server.js
@@ -6,6 +6,23 @@ app.get('/', function (req, res) {
     res.send('Hello World First Express API')
 })
 
+app.get('/api', function (req, res) {
+    //passing data using query string : key value pairs that are typed after "?"
+    //http://localhost:9000/api?name=padmaja&session=mernstack
+    let qsObbj = req.query; //query string object as a request
+    res.json(qsObbj)  
+})
+
+//seding data in route param
+app.get('/getinfo/:productid/details', function (req, res) {
+    //http://localhost:9000/getinfo/29/details
+    let routeParam = req.params["productid"]; //readin value coming in as request route
+    routeParam == 25 ? 
+    res.send("Awsome Product") 
+    :
+    res.send("Product is not available") 
+})
+
 app.get('/second', function (req, res) {
     res.send('Hello World Second Express API')
 })
